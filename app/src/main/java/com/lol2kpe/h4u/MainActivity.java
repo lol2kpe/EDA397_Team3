0a59729310e10c14efbb00fd1f7b5e89076dee77
Jonathan <jongrans@student.chalmers.se>
2017-04-02 15:09:49 +0200

    Added a toolbar and filter action
    Created a customizable toolbar to replace the current app bar. Also,
    added a "dumb" filter action to the toolbar which, so far, doesn't do
    anything.

--- app/src/main/java/com/lol2kpe/h4u/MainActivity.java
+++ app/src/main/java/com/lol2kpe/h4u/MainActivity.java
@@ -2,6 +2,9 @@ package com.lol2kpe.h4u;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		//Sets the customized toolbar as the app bar for this activity
+		Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu (Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.toolbar, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
