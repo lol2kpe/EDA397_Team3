079cbef41ceb5fe112d9cde3b801fb0f67b9176b
samhal <halali@student.chalmers.se>
2017-04-03 15:19:26 +0200

    add first user-location draft(NOT WORKING)

--- app/src/main/java/com/lol2kpe/h4u/MainActivity.java
+++ app/src/main/java/com/lol2kpe/h4u/MainActivity.java
@@ -1,7 +1,11 @@
 package com.lol2kpe.h4u;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		UserLocationMonitor monitor = UserLocationMonitor.getMonitor(this);
+		Location location;
+		while((location = monitor.getLocation()) == null);
+		Toast.makeText(getApplicationContext(),
+				location.getLatitude() + "\n" + location.getLongitude(),
+				Toast.LENGTH_LONG).show();
 	}
 }
---
+++ app/src/main/java/com/lol2kpe/h4u/UserLocationMonitor.java
@@ -0,0 +1,72 @@
+package com.lol2kpe.h4u;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+/**
+ * Created by sam on 2017-04-03.
+ */
+
+public class UserLocationMonitor {
+    public static final String NETWORK_PROVIDER = LocationManager.NETWORK_PROVIDER;
+    public static final String GPS_PROVIDER = LocationManager.GPS_PROVIDER;
+    //Get the location as frequently as possible
+    private static final long MIN_TIME = 0;
+    private static final float MIN_DISTANCE = 0;
+    private Location location;
+    private LocationManager locationManager;
+    private UserLocationListener userLocationListener = new UserLocationListener();
+
+    public static UserLocationMonitor getMonitor(Activity activity){
+        LocationManager locationManager = (LocationManager) activity
+                .getSystemService(
+                        Context.LOCATION_SERVICE
+                );
+        UserLocationMonitor monitor = new UserLocationMonitor(locationManager)
+                .addProvider(NETWORK_PROVIDER)
+                .addProvider(GPS_PROVIDER);
+        return monitor;
+    }
+    private UserLocationMonitor(LocationManager locationManager){
+        this.locationManager = locationManager;
+    }
+
+    private UserLocationMonitor addProvider(String provider){
+        this.locationManager.requestLocationUpdates(provider,
+                MIN_TIME,
+                MIN_DISTANCE,
+                this.userLocationListener);
+        return this;
+    }
+    //Maybe return new location to freeze
+    public Location getLocation(){
+        return this.location != null ? new Location(this.location) : null;
+    }
+
+    private class UserLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            UserLocationMonitor.this.location = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+}
