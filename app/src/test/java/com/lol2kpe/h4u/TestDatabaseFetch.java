ff33131fb254a7d5265ba21d2d73356bda4ca3ac
davve94 <fodavid@student.chalmers.se>
2017-04-04 16:58:52 +0200

    add test for database fetch

---
+++ app/src/test/java/com/lol2kpe/h4u/TestDatabaseFetch.java
@@ -0,0 +1,29 @@
+package com.lol2kpe.h4u;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davidfogelberg on 2017-04-02.
+ */
+
+public class TestDatabaseFetch {
+
+    @Test
+    public void testGetData() {
+
+        
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
