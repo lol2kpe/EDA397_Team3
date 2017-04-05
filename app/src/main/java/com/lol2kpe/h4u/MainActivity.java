46cb5fe96a71c73a03da09d77ff465c6bb715dfa
davve94 <fodavid@student.chalmers.se>
2017-04-05 16:57:38 +0200

    add dummy data hospitals

--- app/src/main/java/com/lol2kpe/h4u/MainActivity.java
+++ app/src/main/java/com/lol2kpe/h4u/MainActivity.java
@@ -25,13 +25,22 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		hospitals = new ArrayList<>();
+		fetchData();
 
 	}
 
 	// fetches data from server
-	public void fetchData() {
+	public List<Hospital>fetchData() {
 
-		VolleyHelper.getInstance(getApplicationContext()).addToRequestQueue(gsonRequest);
+		Hospital sahlgrenska = new Hospital("Sahlgrenska",5,"Emergency", 57.703830518, 11.93582959, 10,"00:00 - 24:00","Göteborg 41753", "075-8833865");
+		Hospital lundby = new Hospital("Lundby",5,"Regular appointments", 57.707663836 , 11.90916303, 10,"08:00 - 22:00","Göteborg 41753", "075-8866465");
+
+		hospitals.add(sahlgrenska);
+		hospitals.add(lundby);
+
+		return hospitals;
+
+		//VolleyHelper.getInstance(getApplicationContext()).addToRequestQueue(gsonRequest);
 	}
 
 	final GsonRequest gsonRequest = new GsonRequest(url, Hospital[].class, null, new Response.Listener<Hospital[]>() {
